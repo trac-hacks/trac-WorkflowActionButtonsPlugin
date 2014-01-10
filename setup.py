12db080bb7bb4f34aedf02e191e2be0716f9a503
ejucovy <ethan.jucovy@gmail.com>
2014-01-10 12:09:54 -0500

    Use MANIFEST.in instead of package_data
    See http://trac-hacks.org/ticket/11494
    
    python setup.py install => error: Setup script exited with error: can't copy 'workflow_actionbuttons/htdocs/fontawesome': doesn't exist or not a regular file

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ setup(name='trac-WorkflowActionButtons',
       keywords='trac plugin',
       license="BSD",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
-      include_package_data=True,
-      package_data={ 'workflow_actionbuttons': ['templates/*', 'htdocs/*'] },
       zip_safe=False,
       entry_points = """
       [trac.plugins]
